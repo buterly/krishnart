35178a3f4de263bf739e84fda514b543fbd757f6
buterly <butterly095@gmail.com>
2024-07-28 17:33:56 +0530

    added infinite scroll

--- api/views.py
+++ api/views.py
@@ -2,11 +2,23 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from krishnart.models import Drawings
 from .serializers import DrawingsSerializer
+from django.http import HttpResponse
 
 @api_view(['GET'])
 def getData(request):
+    range = int(request.GET['range'])
+    if range < len(Drawings.objects.all()):
+        try:
+            drawings = Drawings.objects.all()[range:range+5]
+        except:
+            drawings = Drawings.objects.all()[range:]
 
-    drawings = Drawings.objects.all()
-    serializer = DrawingsSerializer(drawings, many=True)
+        finally:
+            serializer = DrawingsSerializer(drawings, many=True)
+            print(serializer.data)
+            return Response(serializer.data)
 
-    return Response(serializer.data)
+    else:
+        
+        return HttpResponse(status=404)
+
